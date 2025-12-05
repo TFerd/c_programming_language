3cd57ba4ca8e9dc115707ed7f70711736c0d21c2
Thomas Ferdinand <thomasferdinand96@gmail.com>
2025-12-05 15:17:06 -0500

    refactor 1-3 with symbolic contants

--- chapter1/1-3.c
+++ chapter1/1-3.c
@@ -1,20 +1,19 @@
 #include <stdio.h>
 
+#define LOWER 0
+#define UPPER 100
+#define INTERVAL 5
+
 // Fahrenheit to celsius converter
 int main() {
     float fahr, celsius;
-    float lower, upper, interval;
 
-    lower = 0;
-    upper = 100;
-    interval = 5;
-
-    fahr = lower;
+    fahr = LOWER;
 
     printf("\n\n%s\t%s\n", "Fahr", "Celcius");
-    while (fahr < upper) {
+    while (fahr < UPPER) {
         celsius = (5.0/9.0) * (fahr - 32.0);
         printf("%3.0f\t%6.1f\n", fahr, celsius);
-        fahr = fahr + interval;
+        fahr = fahr + INTERVAL;
     }
 }
