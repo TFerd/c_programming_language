af6c10d76408a14723ce5299caaed901fb21bad7
Thomas Ferdinand <thomasferdinand96@gmail.com>
2025-12-07 16:30:38 -0500

    1-12

---
+++ chapter1/1-12.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+#define OUT 0
+#define IN 1
+
+// Prints input one word per line
+int main()
+{
+    int c;
+    while((c = getchar()) != EOF)
+    {
+        if(c == ' ' || c == '\n' || c == '\t') 
+        {
+            putchar('\n');
+        }
+        else
+        {
+            putchar(c);
+        }
+    }
+}
