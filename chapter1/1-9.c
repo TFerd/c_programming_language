c50fc56d4eebfe78bb29a436873e2f42405fe067
Thomas Ferdinand <thomasferdinand96@gmail.com>
2025-12-07 13:09:38 -0500

    1-9

---
+++ chapter1/1-9.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() 
+{
+    int c;
+    while ((c = getchar()) != EOF) 
+    {
+        if (c == ' ')
+        {
+            putchar(c);
+            while ((c = getchar()) != EOF && c == ' ');
+            putchar(c);
+        }
+        else 
+        {
+            putchar(c);
+        }
+    }
+}
