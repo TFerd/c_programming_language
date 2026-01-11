cf18770543abda478ea5e587106796da1569f5b7
Thomas Ferdinand <thomasferdinand96@gmail.com>
2026-01-10 21:50:17 -0500

    2-10

---
+++ chapter2/2-10.c
@@ -0,0 +1,29 @@
+// 2-10: Rewrite the function lower, which converts upper case letters to lower case,
+// with a conditional expression instead of if-else.
+#include <stdio.h>
+
+void lower(char s[]);
+
+int main()
+{
+	char s[] = "AsdfASDFasdfAFsadf";
+	lower(s);
+
+	int i;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		printf("%c", s[i]);
+	}
+	printf("\n");
+
+	return 0;
+}
+
+void lower(char s[])
+{
+	int i;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		s[i] = (s[i] >= 'A' && s[i] <= 'Z') ? s[i] - 'A' + 'a' : s[i];
+	}
+}
